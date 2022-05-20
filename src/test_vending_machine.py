ac250445967b632e7ab50269b4fb7d95613a3936
Your Name <sfauvel@gmail.com>
2022-05-20 08:52:35 +0200

    Initialize project

---
+++ src/test_vending_machine.py
@@ -0,0 +1,19 @@
+"""
+This file is an example of using doc as test with pytest.
+"""
+import os
+import pytest
+from doc_as_test_pytest import DocAsTest, doc, doc_module
+
+
+
+def test_introduction(doc):
+    """
+    In this exercise you will build the brains of a vending machine. 
+    It will accept money, make change, maintain inventory, and dispense products. 
+    All the things that you might expect a vending machine to accomplish. 
+    Features are detailed below.
+    """
+
+    doc.write("You can write in your test the information you want to see in your documentation.")
+
